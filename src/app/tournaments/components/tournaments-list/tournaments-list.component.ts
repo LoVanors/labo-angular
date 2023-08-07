c6a4baa2ad3b9630e3cec8cc6a6bbce81b541b34
loic vanorshoven <loic.vanorshoven@hotmail.com>
2023-08-07 21:54:50 +0200

    les select posent des problèmes

--- src/app/tournaments/components/tournaments-list/tournaments-list.component.ts
+++ src/app/tournaments/components/tournaments-list/tournaments-list.component.ts
@@ -1,11 +1,12 @@
 import {Component, OnInit} from '@angular/core';
 import {TournamentService} from "../../services/tournament.service";
-import {Observable, tap,} from "rxjs";
+import {delay, filter, Observable, tap,} from "rxjs";
 import {TournamentIndexDTO} from "../../models/tournamentIndexDTO";
 import {TournamentDTO} from "../../models/tournamentDTO";
 import {AutoCompleteCompleteEvent} from "primeng/autocomplete";
 import {Router} from "@angular/router";
 import {FormBuilder, FormGroup} from "@angular/forms";
+import {TournamentStatus} from "../../enums/tournamentStatus";
 
 @Component({
   selector: 'app-tournaments-list',
@@ -15,20 +16,20 @@ import {FormBuilder, FormGroup} from "@angular/forms";
 export class TournamentsListComponent implements OnInit {
 
   tournamentsList$!: Observable<TournamentIndexDTO>;
-  tournaments: TournamentDTO[]|null = [];
-  filterByFormActive: boolean=false;
+  tournaments: TournamentDTO[] | null = [];
+  filterByFormActive: boolean = false;
   filterByForm: FormGroup;
-  filteredTournaments!: TournamentIndexDTO;
+  spinner: boolean = false;
 
   constructor(private _tournamentsService: TournamentService,
               private _FB: FormBuilder,
               private _router: Router) {
     this.filterByForm = this._FB.group(
       {
-        name: [null,[]],
-        categories: [null,[]],
-        status: this._FB.array([],[]),
-        womenOnly: [null,[]]
+        name: [null],
+        categories: [null],
+        status: [null],
+        womenOnly: [null]
       }
     )
   }
@@ -45,9 +46,14 @@ export class TournamentsListComponent implements OnInit {
   }
 
 
-  private initObservables(){
-    this.tournamentsList$ = this._tournamentsService.getTournamentsFromServer().pipe(
-      tap( data => this.tournaments= data.results)
+  private initObservables() {
+    this.tournamentsList$ = this._tournamentsService.getTournamentsFromServer(
+      {
+        status: [TournamentStatus.WAITINGFORPLAYERS, TournamentStatus.INPROGRESS,TournamentStatus.CLOSED ],
+      }
+    ).pipe(
+      tap(data => {this.tournaments = data.results
+      console.log(data.results)})
     );
   }
 
@@ -55,9 +61,44 @@ export class TournamentsListComponent implements OnInit {
     this.filterByFormActive = !this.filterByFormActive;
   }
 
-  findByFilter(){
-    this.filteredTournaments = this.filterByForm.value;
-    console.log(this.filteredTournaments);
+  resetFilter() {
+    this.filterByForm.reset();
+    this.spinner=true;
+    setInterval(()=>this.spinner=false, 1100);
+    setInterval(()=>location.reload(), 1000);
   }
+
+  findByFilter() {
+    if (this.filterByForm.valid) {
+      let name = this.filterByForm.get('name')?.value;
+      let category = this.filterByForm.get('category')?.value === null ? null : this.filterByForm.get('category')?.value;
+      let status = this.filterByForm.get('status')?.value;
+      let selectedStatus: string[] = [];
+      let womenOnly = this.filterByForm.get('womenOnly')?.value;
+
+      if (status) {
+        Object.keys(status).forEach(
+          key => {
+            selectedStatus.push(status[key].name);
+          }
+        );
+      }
+
+      this.tournamentsList$ = this._tournamentsService.getTournamentsFromServer(
+        {
+          name,
+          category,
+          status: selectedStatus,
+          womenOnly
+        }
+      ).pipe(
+        tap(data =>{ this.tournaments = data.results
+          this.spinner=true}),
+        delay(1000),
+        tap(() => this.spinner=false)
+      )
+    }
+  }
+
 
 }
--- src/app/tournaments/services/tournament.service.ts
+++ src/app/tournaments/services/tournament.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpParams} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {environment} from "../../environments/environment";
 import {TournamentIndexDTO} from "../models/tournamentIndexDTO";
@@ -12,13 +12,36 @@ import {AuthService} from "../../core/services/auth.service";
 export class TournamentService {
 
   constructor(private _http: HttpClient,
-              private _authServ: AuthService) { }
-
-  getTournamentsFromServer():Observable<TournamentIndexDTO> {
-   return  this._http.get<TournamentIndexDTO>(`${environment.apiURL}/Tournament`);
+              private _authServ: AuthService) {
   }
 
-  addTournamentToServer(newTournament: TournamentAddDTO):Observable<TournamentAddDTO>{
+  getTournamentsFromServer(params: {
+    name?: string;
+    category?: string;
+    status?: string[];
+    womenOnly?: boolean;
+  }): Observable<TournamentIndexDTO> {
+    let httpParams = new HttpParams();
+    if (params.name) {
+      httpParams = httpParams.set('Name', params.name);
+    }
+    if (params.category) {
+      httpParams = httpParams.set('Category', params.category);
+    }
+    if (params.status) {
+      params.status.forEach(sta => {
+        httpParams = httpParams.append('Statuses', sta);
+      })
+    }
+    if (params.womenOnly) {
+      httpParams = httpParams.set('WomenOnly', params.womenOnly);
+    }
+
+    return this._http.get<TournamentIndexDTO>(`${environment.apiURL}/Tournament`,
+      {params: httpParams});
+  }
+
+  addTournamentToServer(newTournament: TournamentAddDTO): Observable<TournamentAddDTO> {
     return this._http.post<TournamentAddDTO>(`${environment.apiURL}/Tournament`, newTournament);
   }
 
